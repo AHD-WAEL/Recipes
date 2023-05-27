615504557223aad46a31493239301b68aec35350
Ahmed <ahmed@Ahmeds-MacBook-Pro.local>
2023-05-27 16:01:52 +0300

    add coding keys for variables

--- Recipes/Model/RecipesCategoriesModel.swift
+++ Recipes/Model/RecipesCategoriesModel.swift
@@ -15,34 +15,56 @@ struct Response: Decodable {
 struct Result:Decodable{
     let id:Int
     let name: String
-    let thumbnail_url: String
+    let thumbnailUrl: String
     let credits: [Brand]
-    let original_video_url: String?
-    let video_url: String?
-    let num_servings: Int?
+    let originalVideoUrl: String?
+    let videoUrl: String?
+    let numServings: Int?
     let instructions: [Instruction]?
     let sections: [Section]?
-    let yields:String?
+    let yields: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case originalVideoUrl = "original_video_url"
+        case videoUrl = "video_url"
+        case numServings = "num_servings"
+        case thumbnailUrl = "thumbnail_url"
+        case id, name, credits, instructions, sections, yields
+    }
 }
 
 struct Instruction: Decodable {
     var appliance: String?
-    var end_time: Int
+    var endTime: Int
     var temperature: Int?
-    var id:Int
+    var id: Int
     var position: Int
-    let display_text: String
-    let start_time: Int
+    let displayText: String
+    let startTime: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case startTime = "start_time"
+        case endTime = "end_time"
+        case displayText = "display_text"
+        case appliance, temperature, id, position
+    }
 }
 
-
 struct Ingredient: Decodable {
-    var created_at: Int
-    var display_plural: String
+    var createdAt: Int
+    var displayPlural: String
     var id: Int
-    var display_singular: String
-    var updated_at: Int
+    var displaySingular: String
+    var updatedAt: Int
     var name: String
+    
+    enum CodingKeys: String, CodingKey {
+        case displaySingular = "display_singular"
+        case updatedAt = "updated_at"
+        case displayPlural = "display_plural"
+        case createdAt = "created_at"
+        case name, id
+    }
 }
 
 struct Section: Decodable {
@@ -55,9 +77,15 @@ struct Component: Decodable {
     var id:Int
     var position: Int
     var measurements: [Measurement]
-    var raw_text:String
-    var extra_comment: String
+    var rawText:String
+    var extraComment: String
     var ingredient: Ingredient
+    
+    enum CodingKeys: String, CodingKey {
+        case rawText = "raw_text"
+        case extraComment = "extra_comment"
+        case id, position, measurements, ingredient
+    }
 }
 
 struct Measurement: Decodable {
@@ -69,15 +97,26 @@ struct Measurement: Decodable {
 struct Unit: Decodable {
     var system: String
     var name: String
-    var display_plural: String
-    var display_singular:String
+    var displayPlural: String
+    var displaySingular:String
     var abbreviation: String
+    
+    enum CodingKeys: String, CodingKey {
+        case displaySingular = "display_singular"
+        case displayPlural = "display_plural"
+        case name, system, abbreviation
+    }
 }
 
 struct Brand: Decodable {
-    let image_url: String?
+    let imageUrl: String?
     let name: String
     let id: Int?
     let slug: String?
     let type: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case imageUrl = "image_url"
+        case name, slug, id, type
+    }
 }
